04c6be7381d0078ff5a1e7da872732d378b5a892
Aaron Blackwell <blackwell@anth.ucsb.edu>
2018-06-21 16:45:28 +0200

    Updated URL and attach dmessage

--- R/LocalGrowth.R
+++ R/LocalGrowth.R
@@ -32,5 +32,8 @@ NULL
 "Welcome to localgrowth (version ",utils::packageVersion('localgrowth'),").
 Basic information can be found in help('localgrowth'),
 with a more detailed walkthrough in vignette('localgrowth').
-Type citation('localgrowth') for ciration information.")
+Type citation('localgrowth') for citation information.
+You can find the latest version and report issues here:
+https://github.com/adblackwell/localgrowth.
+Install the latest version with devtools::install_github('adblackwell/localgrowth')")
 }
