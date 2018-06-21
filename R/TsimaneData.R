696f96c5ca8063852615ef8db5e525d096fa72bb
Aaron Blackwell <blackwell@anth.ucsb.edu>
2018-06-21 16:03:17 +0200

    Updated for package check fixes. Renamed some files and reorganized.

---
+++ R/TsimaneData.R
@@ -0,0 +1,15 @@
+#' A subset of Tsimane growth data.
+#'
+#' A dataset containing 200 Tsimane growth measurements.
+#'
+#' @format A data frame with 200 rows and 6 variables:
+#' \describe{
+#'   \item{PID}{Individual identifier}
+#'   \item{Sex}{Sex, "Male" or "Female"}
+#'   \item{Age}{Age in years}
+#'   \item{Height}{Height in centimeters}
+#'   \item{Weight}{Weight in kilograms}
+#'   \item{BMI}{Body mass index in kg/m^2}
+#' }
+#' @source Blackwell, AD, Urlacher, SS, Beheim, B, von Rueden, C, Jaeggi, A, Stieglitz, J, Tumble, BC, Gurven, MD, Kaplan, H. (2017) Growth references for Tsimane forager-horticulturalists of the Bolivian Amazon, American Journal of Physical Anthropology. 162(3) 441-461. DOI: 10.1002/ajpa.23128.
+"TsimaneData"
